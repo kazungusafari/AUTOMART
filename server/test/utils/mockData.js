d4e46fd9980d4492d0a2e587defdc84ea362580a
Kazungu Safari <peter.kazungu.safari@gmail.com>
2019-05-24 11:54:39 +0300

    [Feature  #166148815] added tests for POST /api/v1/auth/signup endpoint

---
+++ server/test/utils/mockData.js
@@ -0,0 +1,29 @@
+import moment from 'moment';
+export default {
+    signup: {
+    validUserDetails: {
+      firstname: 'John',
+      lastname: 'Doe',
+      address: '100,00100,Nairobi',
+      email: 'example@gmail.com',
+      password: 'password100',
+      confirmPassword: 'password100'
+    },
+    adminUserDetails: {
+      firstname: 'John',
+      lastname: 'Doe',
+      email: 'admin@email.com',
+      address: '55,01025,Kisumu',
+      password: 'password100',
+      confirmPassword: 'password100'
+    },
+    invalidUserDetails: {
+      firstname: '',
+      lastname: 'Jane',
+      email: 'example@gmail.com',
+      address: '99,00101,Eldoret',
+      password: 'password',
+      confirmPassword: 'pass'
+    },
+   }
+ };
