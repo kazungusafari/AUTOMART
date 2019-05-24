3dfe9bbab89e91624e9ce545f7a38850e9ea48b1
Kazungu Safari <peter.kazungu.safari@gmail.com>
2019-05-24 15:04:40 +0300

    [Feature #166149568] added a login controller

--- server/src/controllers/UserController.js
+++ server/src/controllers/UserController.js
@@ -1,5 +1,5 @@
 import { config } from 'dotenv';
-import User from '../models/user';
+import User from '../models/isUserRegistered';
 import Authorization from '../middlewares/Authorization';
 
 config();
@@ -22,18 +22,18 @@ class UserController {
     const isUserRegistered = User.findOne(req.body.email);
 
     if (!isUserRegistered) {
-      const user = User.create(req.query, req.body);
-      const token = Authorization.generateToken(user);
+      const isUserRegistered = User.create(req.query, req.body);
+      const token = Authorization.generateToken(isUserRegistered);
       return res.status(201).send(
         {
           status: res.statusCode,
           data: {
             token,
-            id: user.id,
-            email: user.email,
-            firstname: user.firstname,
-            lastname: user.lastname,
-            isAdmin: JSON.parse(user.isAdmin),
+            id: isUserRegistered.id,
+            email: isUserRegistered.email,
+            firstname: isUserRegistered.firstname,
+            lastname: isUserRegistered.lastname,
+            isAdmin: JSON.parse(isUserRegistered.isAdmin),
           },
         },
       );
@@ -45,6 +45,65 @@ class UserController {
       error: 'Email is already registered',
     });
   }
+
+  
+  /**
+   * Logs in a isUserRegistered
+   * @method login
+   * @memberof UserController
+   * @param {object} req
+   * @param {object} res
+   * @returns {(function|object)} Function next() or JSON object
+   */
+
+   static async loginUser(req, res) {
+    const { email, password } = req.body;
+    const isUserRegistered = User.findOne(email);
+
+    if (!isUserRegistered) {
+      return res.status(404).send({
+        status: res.statusCode,
+        error: 'Email is not registered',
+      });
+     }
+    const isPasswordValid = UserController.verifyPassword(password, isUserRegistered.password);
+
+    if (!isPasswordValid) {
+       return res.status(401).send({
+      status: res.statusCode,
+      error: 'Password is wrong',
+    });
+      
+    }
+    const token = Authorization.generateToken(isUserRegistered);
+    return res.status(200).send(
+        {
+          status: res.statusCode,
+          data: {
+            token,
+            id: isUserRegistered.id,
+            email: isUserRegistered.email,
+            firstname: isUserRegistered.firstname,
+            lastname: isUserRegistered.lastname,
+            isAdmin: JSON.parse(isUserRegistered.isAdmin),
+          },
+        },
+      );
+  }
+
+    /**
+   * @method verifyPassword
+   * @memberof UserController
+   * @param {string} password
+   * @param {string} hash
+   * @return {Promise} Promise of true or false
+   */
+  static verifyPassword(password, hash) {
+    return bcrypt.compareSync(password, hash);
+  }
+
+
+
 }
 
 export default UserController;
