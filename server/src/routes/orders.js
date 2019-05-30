b7165c6c1ffdb1ae91d12c0280acb79a53867995
Kazungu Safari <peter.kazungu.safari@gmail.com>
2019-05-30 23:38:46 +0300

    feature(documentation): add documentation for the PATCH /api/v1/order/<:id>/price route [Feature  #166150626]

--- server/src/routes/orders.js
+++ server/src/routes/orders.js
@@ -12,3 +12,134 @@ orderRoutes.post('/', OrderValidation.createOrder, validation, OrderController.c
 orderRoutes.patch('/:id/price', OrderValidation.updateOrderPrice, validation, OrderController.UpdatePrice);
 
 export default orderRoutes;
+
+/**
+ * @swagger
+ *
+ * /api/v1/order/:
+ *   post:
+ *     description: Create a purchase order
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: order
+ *         description: order object
+ *         in: body
+ *         required: true
+ *         type: string
+ *         schema:
+ *           $ref: '#/definitions/NewOrder'
+ *     responses:
+ *       201:
+ *         description: Created
+ *         schema:
+ *           $ref: '#/definitions/Order'
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not found
+ *     security:
+ *       - apiKey : []
+ *
+ *
+ */
+
+/**
+ * @swagger
+ * definitions:
+ *   NewOrder:
+ *     type: object
+ *     required:
+ *       - status
+ *       - price
+ *       - offeredPrice
+ *       - carId
+ *
+ *     properties:
+ *       offeredPrice:
+ *         type: int
+ *         format: int64
+ *       status:
+ *         type: string
+ *         enum:
+ *           - sold
+ *           - available
+ *       price:
+ *         type: int
+ *         format: int64
+ *       carId:
+ *         type: int
+ *         format: int64
+ *
+ *
+ *
+ *
+ *
+ *
+ *   Order:
+ *     type: object
+ *     required:
+ *       - id
+ *       - createdOn
+ *       - carId
+ *       - offeredPrice
+ *       - price
+ *       - status
+ *
+ *     properties:
+ *       id:
+ *         type: int
+ *       createOn:
+ *         type: datetime
+ *       carId:
+ *         type: int
+ *         format: int64
+ *       price:
+ *         type: int
+ *         format: int64
+ *       offeredPrice:
+ *         type: int
+ *         format: int64
+ *       status:
+ *         type: string
+ *
+ *
+ *
+ */
+
+/**
+ * @swagger
+ *
+ * /api/v1/order/<:id>/price:
+ *   patch:
+ *     description: Update the price of a car..
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         description: Id of the specific car to update
+ *         in: path
+ *         type: int
+ *       - name : authorization
+ *         description: Access token for authentication
+ *         in : header
+ *         type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ *         schema:
+ *           $ref: '#/definitions/Car'
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not found
+ *     security:
+ *       - apiKey : []
+ *
+ *
+ */
