8635a695375646182bfa3ec62e9d191d24972b70
Kazungu Safari <peter.kazungu.safari@gmail.com>
2019-05-31 13:45:28 +0300

    bug(tests): correctly add request queries to requests [Finishes  #166369381]

--- server/test/routes/cars/getAllAdsInAgivenPriceRange.js
+++ server/test/routes/cars/getAllAdsInAgivenPriceRange.js
@@ -10,10 +10,11 @@ import tokens from '../../utils/tokens';
 const { userToken } = tokens;
 
 
-describe('Car Routes: unsold cars', () => {
+describe('Car Routes: unsold cars within a price range', () => {
   it('get all  unsold cars within a price range.', (done) => {
     request(app)
-      .get(encodeURI('/api/v1/car?status=available&min_price=​1000000​&max_price=​1500000'))
+      .get(encodeURI('/api/v1/car'))
+      .query({ min_price: 1000000, max_price: 3000000, status: 'available' })
       .set('Accept', 'application/json')
       .set('authorization', `Bearer ${userToken}`)
       .end((err, res) => {
@@ -26,25 +27,25 @@ describe('Car Routes: unsold cars', () => {
   it('should return error if no unsold cars within a given range is found', (done) => {
     request(app)
       // eslint-disable-next-line no-irregular-whitespace
-      .get(encodeURI('/api/v1/car?status=available&min_price=​10000000&max_price=​1500000000'))
+      .get(encodeURI('/api/v1/car'))
       .set('Accept', 'application/json')
       .set('authorization', `Bearer ${userToken}`)
+      .query({ max_price: 4000000, min_price: 15000000, status: 'available' })
       .end((err, res) => {
         expect(res.statusCode).to.equal(404);
-        expect(res.body.errors[0].msg).to.equal('Not Found');
+        expect(res.body.error).to.equal('Not Found');
         done();
       });
   });
 
   it('should return error for unauthorized access', (done) => {
     request(app)
-      .get(encodeURI('/api/v1/car?status=available&min_price=​100000​&max_price=​1500000'))
+      .get(encodeURI('/api/v1/car'))
       .set('Accept', 'application/json')
       .set('authorization', '')
+      .query({ max_price: 1000000, min_price: 3000000, status: 'available' })
       .end((err, res) => {
         expect(res.statusCode).to.equal(401);
-        expect(res.body).to.be.a('object');
-        expect(res.body).to.include.keys('error');
         expect(res.body.error).to.equal('Unauthorized user');
 
         done();
