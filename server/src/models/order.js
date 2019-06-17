dde97eda2799d87df474e8ada5df2b05792cb65a
Kazungu Safari <peter.kazungu.safari@gmail.com>
2019-06-17 08:42:43 +0300

    feature(model): modify order model  for POST /api/v1/order/ [Starts  #166717471]

--- server/src/models/order.js
+++ server/src/models/order.js
@@ -1,73 +1,71 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-shadow */
 /* eslint-disable no-shadow */
 /* eslint-disable no-tabs */
 /* eslint-disable eqeqeq */
 import moment from 'moment';
 import { config } from 'dotenv';
-import orderData from './data/orderData';
+import db from './index';
 
 const dateTime = moment().format('YYYY-MM-DD h:m:s');
 
-
 config();
-
-const { registeredSaleOrders } = orderData;
-
 
 class Order {
   /**
-   * class constructor
-   *
-   */
-  constructor() {
-    
-    this.orders = registeredSaleOrders;
-    
-  }
-
-  /**
+   * Create a new order in the database
    * @param {integer} orderId id of the car
    * @param {object} data the order details
    * @returns {object} order object
    */
   create(userId, data) {
-    const newOrder = {
-      id: registeredSaleOrders.length + 1,
-      carId: data.carId || '',
-      owner: userId,
-      createdOn: dateTime || '',
-      status: data.status || '',
-      price: data.price || '',
-      offeredPrice: data.offeredPrice || '',
-      modifiedDate: null,
-    };
-    this.orders.push(newOrder);
-    return newOrder;
+    const order = [
+      userId,
+      data.carId,
+      data.price,
+      data.offeredPrice,
+      data.status,
+      dateTime,
+      null,
+    ];
+
+    const text = `INSERT INTO
+      orders(owner, carId, price,offeredPrice,status,createdDate,modifiedDate)
+      VALUES ($1, $2, $3,$4,$5,$6,$7) returning *`;
+
+    const response = db.query(text, order);
+    return response;
   }
 
 
   /**
-   *
-   * @param {integer} id
+   * Find an order in the database by id
+   * @param {integer} id the id of the order
    * @returns {object} order object
    */
   findOneOrder(id) {
-    // eslint-disable-next-line radix
-    return this.orders.find(order => order.id === id);
+    const text = 'SELECT * FROM orders WHERE id = $1';
+    const response = db.query(text, [id]);
+    return response;
   }
 
 
   /**
-   *
+   * Update the buying price of the order
    * @param {integer} id id of the order
    * @param {integer} price new offered price
    * @returns {object} order object
    */
   update(id, price) {
-    const order = this.findOneOrder(id);
-    const index = this.orders.indexOf(order);
-    this.orders[index].modifiedOn = dateTime;
-    this.orders[index].offeredPrice = price;
-    return this.orders[index];
+    const updateQuery = `UPDATE orders
+      SET price=$1, modifiedOn=$2 WHERE id=$3 returning *`;
+    const details = [
+      price,
+      dateTime,
+      id,
+    ];
+    const response = db.query(updateQuery, details);
+    return response;
   }
 }
 
